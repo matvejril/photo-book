b4dbbb854ac3018631c6bdc2b826625058e2b66e
MatvejRil <matvej.ril@gmail.com>
2017-09-13 14:59:47 +0700

    float footer

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+// var gulp         = require('gulp'),
+//     less         = require('gulp-less'),
+//     autoprefixer = require('gulp-autoprefixer');
+//
+// gulp.task('less', function() {
+//     return gulp.src('styles/less/*.less')
+//         .pipe(less())
+//         .pipe(autoprefixer(['last 15 versions', '>1%', 'ie 8', 'ie 7'], {cascade: true}))
+//         .pipe(gulp.dest('styles/css'))
+// });
+//
+// gulp.task('watch', function() {
+//     gulp.watch('styles/less/*.less', [less])
+// });
